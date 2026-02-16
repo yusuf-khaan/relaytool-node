6d670a3dabb57138229c849d2bf8324208d69311
yusuf-khaan <thekhanyusuf096@gmail.com>
2026-02-16 22:50:53 +0530

    fx

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,7 @@ const db = knex({
   client: "pg",
   connection: {
     host: process.env.DB_HOST || "172.25.192.1",
-    port: Number(process.env.DB_PORT || 5433),
+    port: Number(process.env.DB_PORT || 5432),
     database: process.env.DB_NAME || "relayhooks",
     user: process.env.DB_USER || "postgres",
     password: process.env.DB_PASS || "123",
