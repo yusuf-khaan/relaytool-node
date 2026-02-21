62c3b28bfad18e8449934007a88808eb918c930d
yusuf-khaan <thekhanyusuf096@gmail.com>
2026-02-21 17:08:12 +0530

    fx

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   );
 });
 app.use(ErrorHandler);
-const PORT = parseInt(process.env.PORT || "5000");
+const PORT = parseInt(process.env.NODE_PORT || "5000");
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
